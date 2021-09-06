c55968f905d99eb9b5289f56fbd50c7f4edef6f1
up1 <somkiat.p@gmail.com>
2021-09-06 15:37:29 +0700

    Create api /employess/id

--- demoapp/src/main/java/com/example/demoapp/employees/EmployeeController.java
+++ demoapp/src/main/java/com/example/demoapp/employees/EmployeeController.java
@@ -1,10 +1,21 @@
 package com.example.demoapp.employees;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
+
+    @GetMapping("/employees/{id}")
+    public EmployeeResponse getEmployeeById(@PathVariable String id) {
+//       Validate input
+//       Claening data
+        EmployeeResponse employee01 = new EmployeeResponse();
+        employee01.setId(Integer.parseInt(id));
+        employee01.setName("somkiat");
+        return employee01;
+    }
 
     @GetMapping("/employees")
     public EmployeeResponse[] listEmployee() {
