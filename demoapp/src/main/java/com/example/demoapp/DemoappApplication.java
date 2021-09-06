ff99e1cf34352a7d907c4d8592d7f2a3760725df
up1 <somkiat.p@gmail.com>
2021-09-06 14:15:09 +0700

    Count all brans

--- demoapp/src/main/java/com/example/demoapp/DemoappApplication.java
+++ demoapp/src/main/java/com/example/demoapp/DemoappApplication.java
@@ -2,12 +2,25 @@ package com.example.demoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class DemoappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoappApplication.class, args);
+		ConfigurableApplicationContext context
+				= SpringApplication.run(DemoappApplication.class, args);
+		// Beans
+		String[] names = context.getBeanDefinitionNames();
+		for (String name : names) {
+			System.out.println(name);
+		}
+		Arrays.stream(names).forEach(System.out::println);
+
+		System.out.println(context.getBeanDefinitionCount());
+
 	}
 
 }
