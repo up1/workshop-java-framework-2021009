2b3ccd33f2eb2bce2f6051eb7f8b1d075bc53d92
up1 <somkiat.p@gmail.com>
2021-09-07 10:30:03 +0700

    Integrate repository to service

--- demoapp/src/main/java/com/example/demoapp/employees/EmployeeService.java
+++ demoapp/src/main/java/com/example/demoapp/employees/EmployeeService.java
@@ -1,13 +1,25 @@
 package com.example.demoapp.employees;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
     public EmployeeResponse getById(int id) {
-        EmployeeResponse response = new EmployeeResponse();
-        response.setId(id);
-        response.setName("somkiat");
-        return response;
+        Optional<Employee> result = employeeRepository.findById(id);
+        if(result.isPresent()) {
+            // Map data from entity to response
+            EmployeeResponse response = new EmployeeResponse();
+            response.setId(result.get().getId());
+            response.setName(result.get().getName());
+            return response;
+        }
+        throw new RuntimeException("Employee not found id=" + id);
     }
 }
