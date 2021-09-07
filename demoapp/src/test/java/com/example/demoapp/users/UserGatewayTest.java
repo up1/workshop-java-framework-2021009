75fed89bd8be61d86c014e2852fa899623e0096a
up1 <somkiat.p@gmail.com>
2021-09-07 16:59:02 +0700

    Integration test with gateway

--- demoapp/src/test/java/com/example/demoapp/users/UserGatewayTest.java
+++ demoapp/src/test/java/com/example/demoapp/users/UserGatewayTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -14,5 +16,12 @@ public class UserGatewayTest {
 
     @Test
     public void getUserById() {
+        // Act
+        Optional<UserResponse> response = userGateway.getUserById(1);
+        // Assert
+        assertTrue(response.isPresent());
+        assertEquals(1, response.get().getId());
+        assertEquals("Bret", response.get().getUsername());
+
     }
 }
