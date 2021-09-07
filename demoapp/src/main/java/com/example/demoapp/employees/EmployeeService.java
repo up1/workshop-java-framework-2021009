fcc468bfa834cf6e573b22e0a0cb7f171935bc2c
up1 <somkiat.p@gmail.com>
2021-09-07 10:34:24 +0700

    Custom exception

--- demoapp/src/main/java/com/example/demoapp/employees/EmployeeService.java
+++ demoapp/src/main/java/com/example/demoapp/employees/EmployeeService.java
@@ -20,6 +20,6 @@ public class EmployeeService {
             response.setName(result.get().getName());
             return response;
         }
-        throw new RuntimeException("Employee not found id=" + id);
+        throw new EmployeeNotFoundException("Employee not found id=" + id);
     }
 }
