9d00a06b8badc79b7b3cc1a7b932aeaa1ff3b4f3
up1 <somkiat.p@gmail.com>
2021-09-07 11:52:47 +0700

    Test with error exception

---
+++ demoapp/src/test/java/com/example/demoapp/employees/EmployeeControllerFailureTest.java
@@ -0,0 +1,33 @@
+package com.example.demoapp.employees;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class EmployeeControllerFailureTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Test
+    @DisplayName("เกิด error 404 เมื่อค้นหา employee id = 1 ไม่เจอ")
+    public void case01() {
+        // Arrange
+        int id = 1;
+         // Act
+        ErrorResponse result
+                = restTemplate.getForObject("/employees/" + id, ErrorResponse.class);
+        // Assert
+        assertEquals(404, result.getCode());
+        assertEquals("Employee not found id=1", result.getDetail());
+    }
+
+}
